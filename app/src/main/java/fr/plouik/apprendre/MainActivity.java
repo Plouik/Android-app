d10236f2ee1d27c88c871dffebe14e4511d9db67
Plouik <olivier.desaintmartin@reseau.eseo.fr>
2018-03-13 17:00:59 +0100

    Initial commit

---
+++ app/src/main/java/fr/plouik/apprendre/MainActivity.java
@@ -0,0 +1,181 @@
+package fr.plouik.apprendre;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.AutoCompleteTextView;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements OnItemClickListener{
+    /*
+        Déroulement :
+            Affichage du layout MainActivity et choix de la destination
+                Autocompletion à l'aide de l'API google ( https://maps.googleapis.com/maps/api/place/autocomplete/json )
+                    Choix des paramétres dans la fonction autocomplete
+            l'origine de l'itinéraire est donné par la position gps @MapsActivity
+
+            OnClik du button ploup lance l'activité maps avec un bundle contenant la destination uniquement
+
+             A faire :
+         écriture de la google map @location/actualisation => MapsActivity
+
+    */
+        private static final String LOG_TAG = "Google Places Autocomp";
+        private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+        private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+        private static final String OUT_JSON = "/json";
+        private static final String API_KEY = "AIzaSyBmhskS8vv0WgcI51ie4oFt2d2wvyoguio";
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Button btn_get=  findViewById(R.id.button_get);
+            btn_get.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent maps = new Intent(MainActivity.this, MapsActivity.class);
+                    AutoCompleteTextView arriver = findViewById(R.id.autoCompleteTextView_arriver);
+                    // On rajoute un extra
+                    maps.putExtra("destination",arriver.getText().toString());
+
+                    // Puis on lance l'intent !
+                    startActivity(maps);
+                }
+            });
+            AutoCompleteTextView autoCompView = findViewById(R.id.autoCompleteTextView_arriver);
+
+            autoCompView.setAdapter(new GooglePlacesAutocompleteAdapter(this, R.layout.list_item));
+            autoCompView.setOnItemClickListener(this);
+        }
+
+        public void onItemClick(AdapterView adapterView, View view, int position, long id) {
+            String str = (String) adapterView.getItemAtPosition(position);
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        }
+
+        public  ArrayList autocomplete(String input) {
+            ArrayList resultList = null;
+
+            HttpURLConnection conn = null;
+            StringBuilder jsonResults = new StringBuilder();
+            try {
+                StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+                sb.append("?key=" + API_KEY);
+                sb.append("&components=country:fr");
+                sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+
+                URL url = new URL(sb.toString());
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+                // Load the results into a StringBuilder
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                }
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, "Error processing Places API URL", e);
+                return resultList;
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error connecting to Places API", e);
+                return resultList;
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+
+            try {
+                // Create a JSON object hierarchy from the results
+                JSONObject jsonObj = new JSONObject(jsonResults.toString());
+                JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+                // Extract the Place descriptions from the results
+                resultList = new ArrayList(predsJsonArray.length());
+                for (int i = 0; i < predsJsonArray.length(); i++) {
+                    System.out.println(predsJsonArray.getJSONObject(i).getString("description"));
+                    System.out.println("============================================================");
+                    resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Cannot process JSON results", e);
+            }
+
+            return resultList;
+        }
+
+        class GooglePlacesAutocompleteAdapter extends ArrayAdapter implements Filterable {
+            private ArrayList resultList;
+
+            public GooglePlacesAutocompleteAdapter(Context context, int textViewResourceId) {
+                super(context, textViewResourceId);
+            }
+
+            @Override
+            public int getCount() {
+                return resultList.size();
+            }
+
+            @Override
+            public String getItem(int index) {
+                return (String) resultList.get(index);
+            }
+
+            @Override
+            public Filter getFilter() {
+                Filter filter = new Filter() {
+                    @Override
+                    protected FilterResults performFiltering(CharSequence constraint) {
+                        FilterResults filterResults = new FilterResults();
+                        if (constraint != null) {
+                            // Retrieve the autocomplete results.
+                            resultList = autocomplete(constraint.toString());
+
+                            // Assign the data to the FilterResults
+                            filterResults.values = resultList;
+                            filterResults.count = resultList.size();
+                        }
+                        return filterResults;
+                    }
+
+                    @Override
+                    protected void publishResults(CharSequence constraint, FilterResults results) {
+                        if (results != null && results.count > 0) {
+                            notifyDataSetChanged();
+                        } else {
+                            notifyDataSetInvalidated();
+                        }
+                    }
+                };
+                return filter;
+            }
+        }
+    }
+
---
+++ app/src/main/java/fr/plouik/apprendre/MapsActivity.java
@@ -0,0 +1,178 @@
+package fr.plouik.apprendre;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng ;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
+import android.location.Location;
+import org.joda.time.DateTime;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MapsActivity extends FragmentActivity implements
+        OnMyLocationButtonClickListener,
+        OnMyLocationClickListener,
+        OnMapReadyCallback
+         {
+        /*
+            !!!!!!!!!!!!!! destination est gerée par l'activité main !!!!!!!!!!!!!!!!!!!!
+            Autorisation d'accés à la location FINE est nécessaire pour la géolocalisation
+
+            Détermination de l'origne de l'itinéraire via la géolocalisation
+            Création de l'itinéraire via l'API Direction de google
+            Affichage de l'itinaire sur une google map
+         */
+    private static final int overview = 0;
+             private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+             private boolean mPermissionDenied = false;
+
+    private GoogleMap mMap;
+    com.google.maps.model.LatLng origin;
+    String destination,origine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        Bundle extras = getIntent().getExtras();
+        //origin = extras.getString("origin");
+        destination = extras.getString("destination");
+        origine = "Abbeville, France";
+        // destinations= "Avventura Scooter Concessionnaire, Boulevard Galliéni, Issy-les-Moulineaux, France";
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+
+    private DirectionsResult getDirectionsDetails(String origine,String destination,TravelMode mode) {
+        DateTime now = new DateTime();
+        try {
+            return DirectionsApi.newRequest(getGeoContext())
+                    .mode(mode)
+                    .origin(origine)
+                    .destination(destination)
+                    .departureTime(now)
+                    .await();
+        } catch (ApiException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        setupGoogleMapScreenSettings(googleMap);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        mMap.setMyLocationEnabled(true);
+
+        DirectionsResult results = getDirectionsDetails(origine,destination,TravelMode.DRIVING);
+        if (results != null) {
+            addPolyline(results, googleMap);
+            positionCamera(results.routes[overview], googleMap);
+            addMarkersToMap(results, googleMap);
+        }
+    }
+
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+
+    }
+
+
+    private void setupGoogleMapScreenSettings(GoogleMap mMap) {
+        mMap.setBuildingsEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setTrafficEnabled(true);
+        UiSettings mUiSettings = mMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setRotateGesturesEnabled(true);
+    }
+
+    private void addMarkersToMap(DirectionsResult results, GoogleMap mMap) {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results.routes[overview].legs[overview].startLocation.lat,results.routes[overview].legs[overview].startLocation.lng)).title(results.routes[overview].legs[overview].startAddress));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results.routes[overview].legs[overview].endLocation.lat,results.routes[overview].legs[overview].endLocation.lng)).title(results.routes[overview].legs[overview].endAddress).snippet(getEndLocationTitle(results)));
+    }
+
+    private void positionCamera(DirectionsRoute route, GoogleMap mMap) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(route.legs[overview].startLocation.lat, route.legs[overview].startLocation.lng), 12));
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[overview].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+    }
+
+    private String getEndLocationTitle(DirectionsResult results){
+        return  "Time :"+ results.routes[overview].legs[overview].duration.humanReadable + " Distance :" + results.routes[overview].legs[overview].distance.humanReadable;
+    }
+
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext
+                .setQueryRateLimit(3)
+                .setApiKey(getString(R.string.google_maps_key))
+                .setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
+
+}
